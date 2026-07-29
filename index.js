c9be1e02509e5a5d1d50cb9a3d8d44d88e67a294
hnavarroo <hnavarro6233@utm.edu.ec>
2026-07-29 15:10:59 -0500

    Fix: servir frontend desde la ruta raiz

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const ticketsRoutes = require('./routes/tickets');
@@ -9,6 +10,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+// Redirección explícita a tu frontend
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
 
 app.use('/tickets', ticketsRoutes);
 
